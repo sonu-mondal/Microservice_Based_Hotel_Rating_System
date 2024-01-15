64dba469a5a671e01e827465ce7db07b5f3fe257
Sonu Mondal <sonu.mondal4598@gmail.com>
2024-01-15 23:38:20 +0530

    Intial commit for hotel service

---
+++ HotelMS/src/main/java/com/hotel/HotelMS/HotelMsApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.HotelMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelMsApplication.class, args);
+	}
+
+}
---
+++ HotelMS/src/test/java/com/hotel/HotelMS/HotelMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.HotelMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

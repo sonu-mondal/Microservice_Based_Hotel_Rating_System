ea92370b5b055f1bdf35cb359631bd225fee6476
Sonu Mondal <sonu.mondal4598@gmail.com>
2024-01-16 16:41:42 +0530

    Intial commit for rating service

---
+++ RatingMS/src/main/java/com/rating/RatingMS/RatingMsApplication.java
@@ -0,0 +1,13 @@
+package com.rating.RatingMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingMsApplication.class, args);
+	}
+
+}
---
+++ RatingMS/src/test/java/com/rating/RatingMS/RatingMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rating.RatingMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

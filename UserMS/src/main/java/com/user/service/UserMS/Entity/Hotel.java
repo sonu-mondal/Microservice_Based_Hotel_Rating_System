43d9092845ee3333eb44ecf8ffd63b1373fe262a
Sonu Mondal <sonu.mondal4598@gmail.com>
2024-01-22 23:15:39 +0530

    resttemplate for calling hotel service

--- UserMS/src/main/java/com/user/service/UserMS/Entity/Hotel.java
+++ UserMS/src/main/java/com/user/service/UserMS/Entity/Hotel.java
@@ -5,17 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Rating {
-	private String ratingId;
-	private String userId;
-	private String hotelId;
-	private int rating;
-	private String feedback;
-	
-	private Hotel hotel;
+@Getter
+@Setter
+public class Hotel {
+	private int hotelId;
+	private String hotelName;
+	private String location;
+	private String about;
 
 }
--- UserMS/src/main/java/com/user/service/UserMS/Service/UserServiceImpl.java
+++ UserMS/src/main/java/com/user/service/UserMS/Service/UserServiceImpl.java
@@ -3,14 +3,16 @@ package com.user.service.UserMS.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.user.service.UserMS.Entity.Hotel;
 import com.user.service.UserMS.Entity.Rating;
 import com.user.service.UserMS.Entity.User;
 import com.user.service.UserMS.Exception.ResourceNotFoundException;
@@ -35,12 +37,19 @@ public class UserServiceImpl implements UserService{
 		return userRepository.save(user);
 	}
 
-	@Override
+	@Override//
 	public List<User> getAllUser() {
-//		List<User> allUser=userRepository.findAll();
+		List<User> allUser=userRepository.findAll();
 //		ArrayList<Rating> ratings=restTemplate.getForObject("http://localhost:9903/ratings/getall/", ArrayList.class);
-//		ratings.stream().map(x);
-		return null;
+//		for(User i:allUser) {
+//			for(Rating r:ratings) {
+//				if(i.getUserId().equals(r.getUserId())) {
+//					i.setRatings(r.getRating());
+//				}
+//			}
+//			
+//		}
+		return allUser;
 	}
 
 	@Override
@@ -56,10 +65,23 @@ public class UserServiceImpl implements UserService{
 		//ArrayList<Rating> forObject=restTemplate.getForObject("http://localhost:9903/ratings/users/8980a1f8-3177-4d38-9947-2ce53f799f6d", ArrayList.class);
 		
 		//Dynamic
-		ArrayList<Rating> forObject=restTemplate.getForObject("http://localhost:9903/ratings/users/"+user.getUserId(), ArrayList.class);
-		 logger.info(forObject);
+		ArrayList<Rating> ratingOfUser=restTemplate.getForObject("http://localhost:9903/ratings/users/"+user.getUserId(), ArrayList.class);
+		 logger.info(ratingOfUser);
+		 
+		 ratingOfUser.stream().map(rating->{
+			 //Api call to hotel service to get the hotel
+			 //http://localhost:9902/hotels/4
+			ResponseEntity<Hotel> getHotel=restTemplate.getForEntity("http://localhost:9902/hotels/"+rating.getHotelId(), Hotel.class);
+			Hotel hotel=getHotel.getBody();
+			logger.info(getHotel.getStatusCode());
+			//set the rating to that hotel
+			rating.setHotel(hotel);
+			 //return the rating
+			return rating;
+			 
+		 }).collect(Collectors.toList());
 		 //now we got particular users rating so we can set the rating details 
-		 user.setRatings(forObject);
+		 user.setRatings(ratingOfUser);
 		return user;
 	}
 
